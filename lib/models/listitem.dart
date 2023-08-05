223303191a79ad6783ccbb2d443ebe72eecd3a30
marcoabat <marco.skupin@abatplus.de>
2023-08-05 02:21:33 +0200

    created models

---
+++ lib/models/listitem.dart
@@ -0,0 +1,12 @@
+class Item {
+  final int id;
+  final String ownerId;
+  String title;
+  String description;
+  final DateTime createdTime;
+  List<String> checkedBy;
+
+  Item(this.id, this.ownerId, this.title, this.description, this.createdTime,
+      List<String>? checkedBy)
+      : checkedBy = checkedBy ?? [];
+}
