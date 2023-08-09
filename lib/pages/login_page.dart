e2b8909919eae617e9623d883b6a30bb6b6a67dd
marcoabat <marco.skupin@abatplus.de>
2023-08-09 17:55:13 +0200

    fixed login screen bug on mobile

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,14 +4,19 @@ import 'package:briessenchecker/widgets/password_textfield.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
   static const routeName = '/login';
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    final TextEditingController emailController = TextEditingController();
-    final TextEditingController passwordController = TextEditingController();
     Size screenSize = MediaQuery.of(context).size;
     double dialogWidth = screenSize.width > 300 ? 300 : screenSize.width;
     return Stack(children: [
